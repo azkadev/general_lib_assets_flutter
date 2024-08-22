6577d44710d796c73965af2fdbe7b1b7e0403cd0
￴￴ <>
2024-08-22 10:23:50 +0700

    update

---
+++ lib/alert_anim.dart
@@ -0,0 +1,7 @@
+class GeneralLibAlertFlareAnimationAssetsFlutter {
+  static const String loading = 'packages/general_lib_assets_flutter/assets/alert/flare/loading.flr';
+  static const String success = 'packages/general_lib_assets_flutter/assets/alert/flare/success_check.flr';
+  static const String error = 'packages/general_lib_assets_flutter/assets/alert/flare/error_check.flr';
+  static const String warning = 'packages/general_lib_assets_flutter/assets/alert/flare/warning_check.flr';
+  static const String info = 'packages/general_lib_assets_flutter/assets/alert/flare/info_check.flr';
+}
